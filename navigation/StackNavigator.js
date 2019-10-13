52cebb902326bf9c754d236422ff3791acd786b0
Troy <troykirin@gmail.com>
2019-10-13 03:17:52 -0700

    init StackNavigator

---
+++ navigation/StackNavigator.js
@@ -0,0 +1,32 @@
+import { createStackNavigator } from "react-navigation-stack";
+import RantScreen from "../screens/RantScreen";
+
+createStackNavigator(RouteConfigs, StackNavigatorConfig);
+
+createStackNavigator({
+  // For each screen that you can navigate to, create a new entry like this:
+  Profile: {
+    // `ProfileScreen` is a React component that will be the main content of the screen.
+    screen: ProfileScreen,
+    // When `ProfileScreen` is loaded by the StackNavigator, it will be given a `navigation` prop.
+
+    // Optional: When deep linking or using react-navigation in a web app, this path is used:
+    path: "people/:name",
+    // The action and route params are extracted from the path.
+
+    // Optional: Override the `navigationOptions` for the screen
+    navigationOptions: ({ navigation }) => ({
+      title: `${navigation.state.params.name}'s Profile'`
+    })
+  },
+
+  Rant: {
+    screen: RantScreen,
+
+    path: "",
+
+    navigationOptions: ({ navigation }) => ({
+      title: `${navigation.state.params.name}'s Profile'`
+    })
+  }
+});
